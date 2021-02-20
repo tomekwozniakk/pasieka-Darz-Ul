7f7415f9be680410b0b093565c2aa7d39b8d2c38
Tomek Wozniak <tomekwozniakk@gmail.com>
2021-02-21 00:46:20 +0100

    fixed a few issues with image slider on about section

--- src/js/index.js
+++ src/js/index.js
@@ -60,11 +60,24 @@ BtnLeft.addEventListener("click", function () {
   }
   circleDiv.classList.add("about__circle-container--active");
   window.setTimeout(resetCircles, 300, true);
-  switcher();
+  activeArticle = document.querySelector(".about__article--active");
+  console.log(activeArticle);
+  let header = activeArticle.querySelector('.about__images');
+  header.innerHTML = `<div class="about__image-container">
+              <img src="/src/assets/img/party_time.jpg" alt="" class="about__image about__image--first">
+            </div>
+            <div class="about__image-container">
+              <img src="/src/assets/img/smoke.jpg" alt="" class="about__image about__image--second">
+            </div>
+            <div class="about__image-container">
+              <img src="/src/assets/img/working_in_sunset.jpg" alt="" class="about__image about__image--third">
+            </div>`;
+            switcher();
 });
 
 BtnRight.addEventListener("click", function () {
   let activeArticle = document.querySelector(".about__article--active");
+  
   let i = articles.indexOf(activeArticle, 0);
   articles[i].classList.remove("about__article--active");
   circles[i].classList.remove("about__circle--active");
@@ -77,44 +90,59 @@ BtnRight.addEventListener("click", function () {
     circles[i + 1].classList.add("about__circle--active");
   }
   circleDiv.classList.add("about__circle-container--active");
-  switcher();
+  activeArticle = document.querySelector(".about__article--active");
+  console.log(activeArticle);
+  let header = activeArticle.querySelector('.about__images');
+  header.innerHTML = `<div class="about__image-container">
+              <img src="/src/assets/img/party_time.jpg" alt="" class="about__image about__image--first">
+            </div>
+            <div class="about__image-container">
+              <img src="/src/assets/img/smoke.jpg" alt="" class="about__image about__image--second">
+            </div>
+            <div class="about__image-container">
+              <img src="/src/assets/img/working_in_sunset.jpg" alt="" class="about__image about__image--third">
+            </div>`;
+            switcher();
 });
 
 // switching images
 
-function switcher() {
-  let activeArticle = document.querySelector('.about__article--active');
-  const imageFirst = activeArticle.querySelector(".about__image--first");
-  const imageSecond = activeArticle.querySelector(".about__image--second");
-  const imageThird = activeArticle.querySelector(".about__image--third");
-
-  function switchImage(image, source, x) {
+function switchImage(image, urls) {
     let sources;
-    sources = source;
+    let timer;
+    sources = urls;
     let i = 0;
-    let timer = setInterval(function () {
+    timer = setInterval(function () {
       if (i >= sources.length) {
         i = 0;
       }
       image.src = sources[i++];
-    }, x);
+    }, 6000);
   }
+
+function switcher() {
+  let activeArticle = document.querySelector(".about__article--active");
+  let imageFirst = activeArticle.querySelector(".about__image--first");
+  let imageSecond = activeArticle.querySelector(".about__image--second");
+  let imageThird = activeArticle.querySelector(".about__image--third");
 
   switchImage(imageFirst, [
     "/src/assets/img/garden.jpg",
     "/src/assets/img/apples.jpg",
     "/src/assets/img/table.jpg",
-  ], 6000);
+  ]);
+  
   switchImage(imageSecond, [
     "/src/assets/img/backyard.jpg",
     "/src/assets/img/bread_sliced.jpg",
     "/src/assets/img/fence.jpg",
-  ], 6000);
+  ]);
+
   switchImage(imageThird, [
     "/src/assets/img/house_sunset.jpg",
     "/src/assets/img/backyard_2.jpg",
     "/src/assets/img/parcel.jpg",
-  ], 6000);
+  ]);
 }
 
 switcher();
