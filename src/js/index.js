c7b800f717b3f0ddb52a7527ca22ca97bcd072cb
Tomek Wozniak <tomekwozniakk@gmail.com>
2021-02-20 07:58:40 +0100

    header, nav and about sections

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,73 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+// slider in "about" section
+
+let articles = [];
+
+function getArticles(){
+    for (let i = 0; i < document.querySelectorAll('.about__article').length; i++){
+        let article = document.querySelectorAll('.about__article')[i];
+        articles.push(article);
+    };
+}
+getArticles();
+
+let circles = [];
+
+function getCircles(){
+    for (let i = 0; i < document.querySelectorAll('.about__circle').length; i++){
+        let circle = document.querySelectorAll('.about__circle')[i];
+        circles.push(circle);
+}}
+
+
+getCircles();
+// getting elements
+
+const BtnLeft = document.querySelector('.about__slider-button--left');
+const BtnRight = document.querySelector('.about__slider-button--right');
+const circleDiv = document.querySelector('.about__circle-container');
+
+
+// animating circles
+function resetCircles(){
+    circleDiv.classList.remove('about__circle-container--active')
+}
+
+// add event listeners on buttons
+
+BtnLeft.addEventListener('click', function(){
+    let activeArticle = document.querySelector('.about__article--active');
+    let i = articles.indexOf(activeArticle, 0);
+    articles[i].classList.remove('about__article--active');
+    circles[i].classList.remove('about__circle--active');
+
+    if (i === 0) {
+        articles[articles.length - 1].classList.add('about__article--active');
+        circles[articles.length - 1].classList.add('about__circle--active');
+    } else {
+        articles[i - 1].classList.add('about__article--active');
+        circles[i - 1].classList.add('about__circle--active');
+    }
+    circleDiv.classList.add('about__circle-container--active');
+    window.setTimeout(resetCircles, 300, true);
+})
+
+BtnRight.addEventListener('click', function(){
+    let activeArticle = document.querySelector('.about__article--active');
+    let i = articles.indexOf(activeArticle, 0);
+    articles[i].classList.remove('about__article--active');
+    circles[i].classList.remove('about__circle--active');
+    
+    if (i === articles.length - 1) {
+        articles[0].classList.add('about__article--active');
+        circles[0].classList.add('about__circle--active');
+    } else {
+        articles[i + 1].classList.add('about__article--active');
+        circles[i + 1].classList.add('about__circle--active');
+    }
+    circleDiv.classList.add('about__circle-container--active');
+    window.setTimeout(resetCircles, 300, true);
+});
+
--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   /* here you can define another js file */
   entry: {
     index: "./src/js/index.js",
-    another: "./src/js/another.js",
+    // another: "./src/js/another.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -82,11 +82,11 @@ module.exports = {
       chunks: ["index"],
       filename: "index.html",
     }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
-      inject: true,
-      chunks: ["index", "another"],
-      filename: "another.html",
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: "./src/pages/another.html",
+    //   inject: true,
+    //   chunks: ["index", "another"],
+    //   filename: "another.html",
+    // }),
   ],
 };
